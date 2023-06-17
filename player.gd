d949d971cd3ac76dee9f938482e0ba9f3b0b312e
SneakyKnifeEars <136655105+SneakyKnifeEars@users.noreply.github.com>
2023-06-16 20:52:21 -0600

    Player scene work + adjustment to input names

--- player.gd
+++ player.gd
@@ -1,11 +1,29 @@
 extends Area2D
 
+@export var speed = 400
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	#set velocity to 0 start of frame
+	var velocity = Vector2.ZERO
+	#get input
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	
+	#if player is moving, animate
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		
+	position += velocity * delta
+	rotation = velocity.angle()
